2db23571bde83e40209bc6c477723307291a5e97
Héctor Valdecantos <hvaldecantos@gmail.com>
2015-04-01 00:52:46 -0400

    add CmdRunner#run method

--- lib/cmd_runner.rb
+++ lib/cmd_runner.rb
@@ -1,7 +1,25 @@
+require 'open3'
+
 class CmdRunner
   attr_reader :wd
 
   def initialize wd = Dir.getwd
     @wd = wd
+  end
+
+  def run cmd
+    cwd = Dir.getwd
+    Dir.chdir @wd
+    begin
+      Open3.popen3(cmd) do |i, o, e, t|
+        error = e.read
+        raise StandardError, error unless (t.value.success? or error.empty?)
+        result = o.read
+      end
+    rescue Errno::ENOENT
+      raise StandardError, "No command '#{cmd}' found\n"
+    ensure
+      Dir.chdir cwd
+    end
   end
 end
--- spec/cmd_runner_spec.rb
+++ spec/cmd_runner_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'open3'
 
 describe "CmdRunner class" do
   it "has a wd as the current working directory" do
@@ -10,5 +11,39 @@ describe "CmdRunner class" do
     directory = Dir.getwd + '/testrepo/curl'
     cr = CmdRunner.new(directory)
     expect(cr).to have_attributes(wd: directory)
+  end
+
+  it "raises an exception when the working directory is not valid" do
+    directory = 'an_invalid_directory'
+    cr = CmdRunner.new(directory)
+    expect{cr.run('ls')}.to raise_error(Errno::ENOENT, /No such file or directory/)
+  end
+
+  it "has the same working directory before and after invoking run method" do
+    cwd = Dir.getwd
+    directory = Dir.getwd + '/testrepo/curl'
+    CmdRunner.new(directory).run('invalid_command') rescue
+    expect(cwd).to eq(Dir.getwd)
+  end
+
+  it "runs the command with open3" do
+    directory = Dir.getwd + '/testrepo/curl'
+    cr = CmdRunner.new(directory)
+    cmd = 'ls'
+    expect(Open3).to receive(:popen3).with(cmd).once
+    cr.run(cmd)
+  end
+
+  it "raises an exception when the command passed is not valid" do
+    directory = Dir.getwd + '/testrepo/curl'
+    cmd = 'invalid_command'
+    cr = CmdRunner.new(directory)
+    expect{cr.run(cmd)}.to raise_error(StandardError, "No command '#{cmd}' found\n")
+  end
+
+  it "runs a command and return the stdout" do
+    directory = Dir.getwd + '/testrepo/curl'
+    cr = CmdRunner.new(directory)
+    expect(cr.run 'pwd').to eq(directory + "\n")
   end
 end
