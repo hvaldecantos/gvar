7f73f320778874c999aa6ef77f6dc7f701e033e1
Héctor Valdecantos <hvaldecantos@gmail.com>
2015-04-02 18:45:13 -0400

    GVarCli can deal with --list-shas

--- lib/g_var_cli.rb
+++ lib/g_var_cli.rb
@@ -1,9 +1,45 @@
+require 'optparse'
 require 'cmd_runner'
 require 'find_cmd'
+require 'list_sha_cmd'
 
 class GVarCLI
-  def self.run opt
+  
+  FIND_SRC_DIRS = '--find-src-dirs'
+  LIST_SHAS = '--list-shas'
+  GVAR_OPTS = [FIND_SRC_DIRS, LIST_SHAS]
+
+  def self.run argv
+    gvar_opts = parse argv
     cr = CmdRunner.new Dir.getwd
-    FindCmd.new(cr).dirs if opt.include? "--find-src-dirs"
+    if gvar_opts.include? FIND_SRC_DIRS
+      FindCmd.new(cr).dirs
+    elsif gvar_opts.include? LIST_SHAS
+      ListShaCmd.new(cr).shas
+    end
+  end
+
+  def self.instanciate_cmd_object gvar_opts
+    cr = CmdRunner.new Dir.getwd
+    if gvar_opts.include? FIND_SRC_DIRS
+      FindCmd.new(cr)
+    elsif gvar_opts.include? LIST_SHAS
+      ListShaCmd.new(cr)
+    end
+  end
+
+  def self.parse argv
+    gvar_opts = []
+    OptionParser.new do |opts|
+      opts.banner = "gvar [--find-src-dirs | --list-shas]"
+      opts.separator "Command line that returns global variables related reports."
+      opts.version = GVar::VERSION
+      opts.on('--find-src-dirs', 'Return a hash with directories containing *.c or *.h files and the number of files.'){ gvar_opts << '--find-src-dirs' }
+      opts.on('--list-shas', 'Return an array of SHA-1 commit identifier')  { gvar_opts << '--list-shas' }
+    end.parse!(argv)
+    if (gvar_opts & GVAR_OPTS).size > 1
+      raise OptionParser::ParseError.new("#{gvar_opts.join(', ')} are mutually exclusive options")
+    end
+    gvar_opts
   end
 end
--- spec/g_var_cli_spec.rb
+++ spec/g_var_cli_spec.rb
@@ -2,8 +2,27 @@ require 'spec_helper'
 
 describe "GVarCLI class" do
   it "prints directories where C src is found" do
-    gvar_opts = ["--find-src-dirs"]
-    expect_any_instance_of(FindCmd).to receive(:dirs).once
-    GVarCLI.run(gvar_opts)
+    [['--find-src-dirs'], ['--find-src-dirs'], FindCmd, :dirs, 
+     ['--list-shas'], ['--list-shas'], ListShaCmd, :shas].each_slice(4) do |argv, gvar_opts, cmd_class, method|
+      expect(GVarCLI).to receive(:parse).once.with(argv).and_return(gvar_opts)
+      expect_any_instance_of(cmd_class).to receive(method).once
+      GVarCLI.run(argv)
+    end
+  end
+
+  it "parses arguments given to gvar command" do
+    argv = ['--find-src-dirs']
+    argv = ['--list-shas']
+    expect(GVarCLI.parse argv).to eq(['--list-shas'])
+  end
+
+  it "raises an exception when an option is not understood" do
+    msg = /invalid option: --not-understandable/
+    expect{ GVarCLI.parse ["--not-understandable"] }.to raise_error(OptionParser::InvalidOption, msg)
+  end
+
+  it "raises an exception if passing mutually exclusive params" do
+    msg = /--find-src-dirs, --list-shas are mutually exclusive options/
+    expect{ GVarCLI.parse ['--find-src-dirs', '--list-shas'] }.to raise_error(OptionParser::ParseError, msg)
   end
 end
