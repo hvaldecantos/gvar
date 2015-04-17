cc12df71c415b5ceed3b91f2e5c814ea330a0069
Héctor Valdecantos <hvaldecantos@gmail.com>
2015-04-17 13:28:48 -0400

    find gv related bugs attempt 2

--- lib/find_bugs_cmd.rb
+++ lib/find_bugs_cmd.rb
@@ -1,9 +1,11 @@
 require 'cmd'
 
 class FindBugsCmd < Cmd
-  COMMAND = "git log %s -- %s/*.c %s/*.h"
+  COMMAND = "git log --unified=0 %s -- %s/*.c %s/*.h"
   def initialize cmd_runner
     super
+    @bug_fix = false
+    @commit = ""
     @bugs = []
   end
 
@@ -18,7 +20,29 @@ class FindBugsCmd < Cmd
 
   private
     def analize line
-      @bugs << line if line.match(/fix/i)
+      commit_match = line.match(/^commit\s(\w*)$/)
+      unless commit_match.nil?
+        # Prints the commit sha1 only if there was a bugfix
+        # puts "Bug fix was in: #{commit_match.captures[0]}" if @bug_fix
+
+        @bugs << "<<<<<<<<<<<<<< #{@commit}" if @bug_fix
+        @bug_fix = false
+        @commit = commit_match.captures[0]
+      end
+
+      # avoid debug, prefixe
+      if @bug_fix == false && line.match(/(^|\s)(fix|issue|bug)/i)
+        @bug_fix = true
+        @bugs << ">>>>>>>>>>>>>> #{line}"
+      end
+
+      if @bug_fix
+        # it is related to a bug only if a global variable appears in a deleted line
+        # A deleted line is also a changed line. A bug fix could never be in an line
+        # that is only added.
+        # puts line if line.match(/^-\s/)
+        @bugs << line if line.match(/^-\s/)
+      end
     end
 
     def default opts = {}
