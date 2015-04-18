e05a1b6f9344bc7ecc53b53b44fefdf3088f2925
Carlos Dubus <carlos.garciadm@gmail.com>
2015-04-18 17:13:15 -0400

    clean code

--- lib/globals_info.rb
+++ lib/globals_info.rb
@@ -1,20 +1,17 @@
 class GlobalsInfo
 	attr_accessor :info
 	def initialize 
-    	@info= {}
-  	end
-
-  	def process_commit commit
-  			
+		@info= {}
+	end
+	
+	def process_commit commit
 		commit[:globals].each do |global_name,global_data|
 			if !@info.key?(global_name)
-
 				@info[global_name] = global_data
 				@info[global_name][:name] = global_name
 				@info[global_name][:first_sha] = commit[:sha]
 				@info[global_name][:bug_count] = 0
 			end
-
 			@info[global_name][:bug_count] += global_data[:bug]
 		end
 	end
