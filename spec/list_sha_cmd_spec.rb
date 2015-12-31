705d16e87173a23771f053938420cb0740c3eb19
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2015-12-31 13:08:54 -0500

    fix list sha command spec

--- spec/list_sha_cmd_spec.rb
+++ spec/list_sha_cmd_spec.rb
@@ -4,6 +4,7 @@ describe "ListShaCmd class" do
   it "returns a list of sha-1 commits from a given option (tag1..tag2)" do
     cr = double(CmdRunner)
     list_sha = ListShaCmd.new(cr)
+    allow(cr).to receive(:wd)
     expect(list_sha).to receive(:analyze_result).once
     list_sha.run
   end
