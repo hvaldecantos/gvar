34b02268c6026cb08f5ca47d25d9fa24f0ec4486
Héctor Valdecantos <hvaldecantos@gmail.com>
2016-02-05 13:41:00 -0500

    add --find-dirs-to-analyze command

---
+++ lib/find_dirs_cmd.rb
@@ -0,0 +1,26 @@
+require 'cmd'
+
+class FindDirsCmd < Cmd
+  COMMAND = "find %s -type f -name '*.c' -or -name '*.h'"
+  def initialize cmd_runner
+    super
+    @dirs = []
+  end
+
+  def run opts = {}
+    opts = default(opts)
+    @cmd = COMMAND % opts[:dirs]
+    analyze_result
+    @dirs.join(" ")
+  end
+
+  private
+    def analize line
+      @dirs |= [(File.dirname(line) + "/*" + File.extname(line)).strip]
+    end
+
+    def default opts = {}
+      opts[:dirs] ||= [@cmd_runner.wd]
+      opts
+    end
+end
